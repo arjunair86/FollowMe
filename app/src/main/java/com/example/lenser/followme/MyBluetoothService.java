08c77b5e2182feca838c0b5f7e4a468c2f9eb690
Arjun <arjunair86@gmail.com>
2017-04-06 23:43:11 +0530

    changed app icon and ImageButtons

--- app/src/main/java/com/example/lenser/followme/MyBluetoothService.java
+++ app/src/main/java/com/example/lenser/followme/MyBluetoothService.java
@@ -1,8 +1,9 @@
 package com.example.lenser.followme;
 
-import android.bluetooth.BluetoothDevice;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,19 +14,17 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 
 import BluetoothFunctions.ConnectedBT;
-import BluetoothFunctions.GetPairedDevices;
 
 import static com.example.lenser.followme.MyBluetoothService.bt;
 import static com.example.lenser.followme.MyBluetoothService.downButton;
@@ -40,13 +39,14 @@ import static com.example.lenser.followme.MyBluetoothService.upButton;
 
 public class MyBluetoothService extends AppCompatActivity {
 
-    public static Button disconnect, upButton, downButton, leftButton, rightButton;
+    public static ImageButton disconnect, upButton, downButton, leftButton, rightButton;
     InputStream inputStream;
     OutputStream outputStream;
     Switch aSwitch;
     public static boolean isAuto = true;
     public static ConnectedBT bt;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,12 +60,12 @@ public class MyBluetoothService extends AppCompatActivity {
         Toolbar toolbar = (Toolbar)findViewById(R.id.connected_toolbar);
         setSupportActionBar(toolbar);
 
-        disconnect = (Button)findViewById(R.id.disconnect);
-        upButton = (Button)findViewById(R.id.upButton);
-        downButton = (Button)findViewById(R.id.downButton);
-        leftButton = (Button)findViewById(R.id.leftButton);
-        downButton = (Button)findViewById(R.id.downButton);
-        rightButton = (Button)findViewById(R.id.rightButton);
+        disconnect = (ImageButton)findViewById(R.id.disconnect);
+        upButton = (ImageButton)findViewById(R.id.upButton);
+        downButton = (ImageButton)findViewById(R.id.downButton);
+        leftButton = (ImageButton)findViewById(R.id.leftButton);
+        downButton = (ImageButton)findViewById(R.id.downButton);
+        rightButton = (ImageButton)findViewById(R.id.rightButton);
 
         aSwitch = (Switch) findViewById(R.id.switch1);
 
